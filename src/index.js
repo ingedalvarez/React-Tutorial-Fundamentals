9c37610ade2130f785a7ea3a01e157c700dc73cd
ingedalvarez <ingedalvarez@gmail.com>
2021-12-07 09:41:04 -0500

    Adding first book example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-function Greeting() {
-  return <h4>This is Edward and this is my first compoment.</h4>;
+function BookList() {
+  return (
+    <section>
+      <Book />
+    </section>
+  );
 }
 
-ReactDom.render(<Greeting />, document.getElementById('root'));
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://m.media-amazon.com/images/I/81255VTWssL._AC_UY218_.jpg"
+      alt="Where the Crawdads Sing"
+    />
+  );
+};
+
+const Title = () => {
+  return <h1>'Where the Crawdads Sing</h1>;
+};
+
+const Author = () => {
+  return <h4>'Delia Owens </h4>;
+};
+
+ReactDom.render(<BookList />, document.getElementById('root'));

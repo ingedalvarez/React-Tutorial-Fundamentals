ebcc06ebcc78d7c8ce63ba41baca8ad376e03a24
ingedalvarez <ingedalvarez@gmail.com>
2021-12-13 11:57:42 -0500

    Booklist component finalizado

--- src/index.js
+++ src/index.js
@@ -5,51 +5,45 @@ import ReactDom from 'react-dom';
 import './index.css';
 
 // VARS
-const firstBook = {
-  img: 'https://m.media-amazon.com/images/I/81255VTWssL._AC_UY218_.jpg',
-  title: 'Where the Crawdads Sing',
-  author: 'Delia Owens',
-};
-
-const secondBook = {
-  img: 'https://m.media-amazon.com/images/I/91nNDY7u-+L._AC_UY218_.jpg',
-  title: 'The Guardians: A Novel',
-  author: 'John Grisham, Michael Beck, et ál.',
-};
+const books = [
+  {
+    id: 1,
+    img: 'https://m.media-amazon.com/images/I/81255VTWssL._AC_UY218_.jpg',
+    title: 'Where the Crawdads Sing',
+    author: 'Delia Owens',
+  },
+  {
+    id: 2,
+    img: 'https://m.media-amazon.com/images/I/91nNDY7u-+L._AC_UY218_.jpg',
+    title: 'The Guardians: A Novel',
+    author: 'John Grisham, Michael Beck, et ál.',
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81lsmyMcYNL._AC_UL200_SR200,200_.jpg',
+    title: 'American Marxism',
+    author: 'Mark R. Levin',
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo aperiam
-          nesciunt velit dicta vel illum minima consectetur eum repudiandae
-          nemo!
-        </p>
-      </Book>
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
 
 const Book = (props) => {
-  const { img, title, author, children } = props;
-  console.log(props);
+  const { img, title, author } = props;
+  // console.log(props);
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
